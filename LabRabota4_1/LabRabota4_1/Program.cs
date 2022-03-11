6d499fdcf0d83a51a94277aee653ff4ea94ae0c5
User#None <77580844+issamansur@users.noreply.github.com>
2022-03-11 05:35:41 +0200

    Add files via upload

---
+++ LabRabota4_1/LabRabota4_1/Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * 10. 8).	В базе данных хранятся номера сотрудников, которым была выписана премия за месяц. 
+ * Проверить, не попал ли кто-либо из сотрудников в эту базу дважды.
+ * 
+ * P.S. Считаем, что список не упорядочен
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LabRabota4_1
+{
+    class Program
+    {
+        static double TimeFunc(int[] array, int number=5)
+        {
+            // секундомер
+            Stopwatch sw = new Stopwatch();
+            // время работы
+            double time = 0;
+            
+            for (int i = 1; i <= number; i++)
+            {
+                // результат повторения персонала в БД
+                bool check = false;
+                sw.Reset();
+                //засекаем время начала работы алгоритма
+                sw.Start();
+
+                if (array.Length > 1)
+                    for (int first = 0; first < array.Length - 1; first++)
+                    {
+                        for (int second = first + 1; second < array.Length; second++)
+                            if (array[first] == array[second])
+                            {
+                                check = true;
+                                break;
+                            }
+
+                        if (check)
+                                break;
+                    }
+                // останавливаем время 
+                sw.Stop();
+                // суммируем время работы алгоритма
+                time += sw.ElapsedMilliseconds;
+            }
+            // определяем среднее время работы алгоритма
+            time /= number;
+
+            return time;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            // список для хранения количества элементов массива
+            List<int> n = new List<int> { 10000, 20000, 30000, 40000, 50000 };
+            double timeWork = 0;
+
+            foreach (int k in n)
+            {
+                Console.WriteLine("----------     Количество элементов массива {0}    ---------- ", k);
+                int[] mas = new int[k];
+                
+
+                // средний случай
+                
+                for (int i = 0; i < mas.Length; i++)
+                    mas[i] = rand.Next(0, 5 * k);
+
+                timeWork = TimeFunc(mas);
+                Console.WriteLine("Время работы алгоритма в среднем случае {0}", timeWork);
+
+                // лучший случай
+                for (int i = 0; i < mas.Length; i++)
+                    mas[i] = i;
+                mas[1] = 0;
+
+                timeWork = TimeFunc(mas);
+                Console.WriteLine("Время работы алгоритма в лучшем случае {0}", timeWork);
+
+                // худший случай
+                for (int i = 0; i < mas.Length; i++)
+                    mas[i] = i;
+
+                timeWork = TimeFunc(mas);
+                Console.WriteLine("Время работы алгоритма в худшем случае {0}", timeWork);
+            }
+            Console.ReadLine();
+        }
+    }
+}
