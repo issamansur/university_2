107df83c31dc710b18ce80fae18d3d235712a4f0
User#None <77580844+issamansur@users.noreply.github.com>
2022-04-22 00:25:30 +0300

    Add files via upload

---
+++ MethodsForSorting2/MethodsForSorting2/Methods.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsForSorting2
+{
+    public static class Methods
+    {
+        // быстрая сортировка
+
+        static int Partition(int[] array, int start, int end)
+        {
+            int marker = start;
+            for (int i = start; i <= end; i++)
+            {
+                if (array[i] <= array[end])
+                {
+                    int temp = array[marker];
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            return marker - 1;
+        }
+
+        static public void Quicksort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                int pivot = Partition(array, start, end);
+                Quicksort(array, start, pivot - 1);
+                Quicksort(array, pivot + 1, end);
+            }
+        }
+
+        // сортивка слиянием
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+
+        static public int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
+        }
+
+        // пирамидальная сортировка
+
+        static int add2pyramid(double[] arr, int i, int N)
+        {
+            int imax;
+            double buf;
+            if ((2 * i + 2) < N)
+            {
+                if (arr[2 * i + 1] < arr[2 * i + 2]) imax = 2 * i + 2;
+                else imax = 2 * i + 1;
+            }
+            else imax = 2 * i + 1;
+            if (imax >= N) return i;
+            if (arr[i] < arr[imax])
+            {
+                buf = arr[i];
+                arr[i] = arr[imax];
+                arr[imax] = buf;
+                if (imax < N / 2) i = imax;
+            }
+            return i;
+        }
+        public static void Sorting(double[] arr, int len)
+        {
+            //step 1: building the pyramid
+            for (int i = len / 2 - 1; i >= 0; --i)
+            {
+                long prev_i = i;
+                i = add2pyramid(arr, i, len);
+                if (prev_i != i) ++i;
+            }
+
+            //step 2: sorting
+            double buf;
+            for (int k = len - 1; k > 0; --k)
+            {
+                buf = arr[0];
+                arr[0] = arr[k];
+                arr[k] = buf;
+                int i = 0, prev_i = -1;
+                while (i != prev_i)
+                {
+                    prev_i = i;
+                    i = add2pyramid(arr, i, k);
+                }
+            }
+        }
+    }
+}
---
+++ MethodsForSorting2/MethodsForSorting2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodsForSorting2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 5, 7, 9, 1, 4, 3, 9, 8, 0 };
+            Methods.Quicksort(array, 0, array.Length - 1);
+            foreach (int x in array)
+                Console.Write(x + " ");
+            Console.WriteLine();
+
+            array = new int[] { 5, 7, 9, 1, 4, 3, 9, 8, 0 };
+            Methods.MergeSort(array, 0, array.Length - 1);
+            foreach (int x in array)
+                Console.Write(x + " ");
+            Console.WriteLine();
+
+            double[] array1 = new double[] { 5, 7, 9, 1, 4, 3, 9, 8, 0 };
+            Methods.Sorting(array1, array1.Length);
+            foreach (int x in array)
+                Console.Write(x + " ");
+        }
+    }
+}
